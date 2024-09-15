21d6febf9dbc1f237dfdd821024ca145dbe1afcd
Exo-nis <fatiheroglu.ceng@gmail.com>
2024-09-16 00:18:53 +0300

    final commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,145 @@
-import Image from "next/image";
-
+const teamMembers = [
+  { name: "Esther Howard", role: "Founder", imgSrc: "/assets/founder.jpg" },
+  { name: "Cody Fisher", role: "Developer", imgSrc: "/assets/developer.jpg" },
+  { name: "Brooklyn Simmons", role: "Designer", imgSrc: "/assets/designer.jpg",},
+];
+const stats = [
+  { number: "350+", label: "Clients Worldwide" },
+  { number: "20+", label: "Team Members" },
+  { number: "100+", label: "Projects Completed" },
+  { number: "85M+", label: "Revenue Generated" },
+];
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <div className="min-h-screen bg-white text-black">
+      {/* İlk görsel ve ortasında başlık */}
+      <div className="relative w-full h-auto">
+        <img
+          src="/assets/hands.jpg"
+          alt="Hands"
+          className="object-cover w-full h-full"
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <div className="absolute inset-0 flex justify-center items-center">
+          <h1 className="text-9xl font-bold text-center px-32 -mt-60">
+            Let's create something great together.
+          </h1>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+      </div>
+
+      {/* 2. kısım başlık ve text */}
+      <div className="px-28 py-40">
+        <h2 className="text-5xl font-bold text-left mb-16">Who we are</h2>
+        <p className="text-4xl text-left">
+          We love what we do and create partnerships with our clients to ensure
+          their digital transformation is positioned for long-term success. We
+          believe that the human dimensions essential to start any successful
+          project and that this is where splendid emotional relationships
+          between the company and people are born.
+        </p>
+      </div>
+      {/* 3. kısım kenarları boşluklu image */}
+      <div className="flex justify-center items-center px-28 pb-60">
+        <img
+          src="/assets/orange.jpg"
+          alt="Centered Image"
+          className="max-w-full h-auto w-full"
+        />
+      </div>
+      {/* 3. kısım Meet heroes */}
+      <div className="px-60 pb-40">
+        <h2 className="text-4xl font-bold text-center pb-20">
+          Meet the heroes behind the magic
+        </h2>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+          {teamMembers.map((member) => (
+            <div className="flex flex-col items-center bg-white rounded-lg shadow-xl overflow-hidden transform scale-90 hover:scale-100 transition-transform duration-200">
+              <div
+                className={`w-full h-auto flex justify-center items-center p-6`}
+              >
+                <img
+                  src={member.imgSrc}
+                  alt={member.name}
+                  className="w-full h-auto "
+                />
+              </div>
+              <div className="p-6 w-full text-left">
+                <h2 className="text-3xl font-semibold pb-4">{member.name}</h2>
+                <p className="text-2xl text-gray-400">{member.role}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      {/*4. kısım istatistik */}
+      <div className="bg-black py-10">
+        <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-24 text-left">
+          {stats.map((stat) => (
+            <div key={stat.label} className="text-white">
+              <h3 className="text-yellow-400 text-6xl font-semibold pb-5">
+                {stat.number}
+              </h3>
+              <p className="text-3xl">{stat.label}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+      {/* 5. kısım görsel ve form */}
+      <div className="min-h-screen flex flex-col md:flex-row items-center justify-center bg-white py-40">
+        {/* Sol Taraftaki Resim */}
+        <div className="flex justify-center items-center w-full md:w-1/2 p-8">
+          <img
+            src="/assets/form-image.jpg"
+            alt="Team Members"
+            className="max-w-full h-auto"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        </div>
+
+        {/* Sağ Taraftaki Form */}
+        <div className="w-full md:w-1/2 pr-20">
+          <h2 className="text-5xl font-bold mb-8 text-right pl-96">
+            We'd love to hear from you
+          </h2>
+          <form className="space-y-4">
+            <div>
+              <input
+                type="text"
+                placeholder="Name*"
+                className="w-full p-4 text-2xl border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-yellow-400"
+              />
+            </div>
+            <div>
+              <input
+                type="email"
+                placeholder="Email*"
+                className="w-full p-4 text-2xl border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-yellow-400"
+              />
+            </div>
+            <div>
+              <input
+                type="url"
+                placeholder="Website URL*"
+                className="w-full p-4 text-2xl border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-yellow-400"
+              />
+            </div>
+            <div>
+              <textarea
+                placeholder="Project Details*"
+                className="w-full p-4 text-2xl border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-yellow-400"
+                rows="5"
+              />
+            </div>
+            <div>
+              <button
+                type="submit"
+                className="w-full p-4 bg-black text-white text-2xl font-semibold rounded-lg hover:bg-gray-700 transition-colors"
+              >
+                Send Proposal
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
